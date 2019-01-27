3bd5b98a7e7fa958d6a694cfd847e7b326bbd3e2
damanrique <46985074+damanrique@users.noreply.github.com>
2019-01-26 21:29:06 -0500

    Add files via upload

--- listas.py
+++ listas.py
@@ -1,19 +1,23 @@
 import random
 
-a=[]
-b=[]
-c=[]
+a = []
+b = []
+c = []
 
-while len(a)<=39:
+while len(a) <= 39:
     ra = random.randint(1, 50)
     rb = random.randint(1, 50)
     a.append(ra)
     b.append(rb)
+# valida los que no estan en a y b y llena la lista c
 
-if ra not in b and rb not in a:
-    c.append(ra)
+ch = 1
 
+for ch in range(1, 50):
+    if ch not in b and ch not in a:
+        c.append(ch)
+        ch = ch + 1
 
-print(a)
-print(b)
-print(c)
+print('la lista a es:', a)
+print('la lista b es:', b)
+print('la lista c es:', c)
